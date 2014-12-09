78b754098be46cf42338455de3e41f5965c32a7d
LC <eternal.kadir@gmail.com>
2014-12-09 14:43:05 +0000

    added test body for RSpecGreeter#greet

--- spec/greeter_spec.rb
+++ spec/greeter_spec.rb
@@ -1,4 +1,8 @@
+require 'spec_helper'
+
 describe 'RSpecGreeter' do
-  it 'RSpecGreeter#greet()' do
+  context 'RSpecGreeter#greet()' do
+    subject(:greeter) { RSpecGreeter.new.greet }  # Given
+    it { is_expected.to match(/Hello RSpec!/) }    # Then
   end
 end
